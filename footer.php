2394c957e6af61154aee20ae93994eb20d57b4c9
João Paulo Cruz <webdevjota@gmail.com>
2018-06-06 14:45:06 -0300

    Estilização do Footer

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<footer class= "mt-3 preto-principal" style="border-top:4px solid #dd1f46;">
+<footer class= "mt-3" style="border-top:4px solid #000; background-color: #0c0c0c;">
 
     <div class="container">
         <div class="row">
@@ -14,17 +14,66 @@
         </div>
     </div>
 
-    <div class="footer-2 vermelho-principal pt-2">
+    <div class="footer-2" style="background-color: #141414; padding-top: 13px;">
         <div class="container">
             <div class="row">
-                <div class="col-md-6 text-light"><p>&copy; <?php echo date('Y'); ?> <a href="<?php echo home_url('/'); ?>"><?php bloginfo('name'); ?></a></p></div>
-
-                <div class="col-md-6 text-light text-right">
-                    Feito por:
+                <div class="col-md-6 text-light copy-left">
+                    <p>&copy; <?php echo date('Y'); ?> <a href="<?php echo home_url('/'); ?>"><?php bloginfo('name'); ?></a></p>
+                    <p>O conteúdo presente nas postagens, como imagens de filmes, series e vídeos são marcas registradas de seus respectivos proprietários.</p>
                 </div>
+
+                <div class="col-md-6 text-light">
+
+                    <div class="cred-logo text-center">
+
+                        <a href="mailto:webdevjota@gmail.com?Subject=Olá%20Jota" target="_top" title="webdejota@gmail.com">
+                        <p class="text-uppercase ">Design e desenvolvimento</p>
+                        <?xml version="1.0" encoding="utf-8"?>
+                        <!-- Generator: Adobe Illustrator 21.0.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+                        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                        viewBox="0 0 100 29" style="enable-background:new 0 0 100 29;" xml:space="preserve">
+                        <style type="text/css">
+                        .st0{fill:#888;}
+                    </style>
+                    <g id="XMLID_3_">
+                        <g id="XMLID_20_">
+                            <path class="st0" d="M37,20.6c-0.6,0-1.1-0.1-1.6-0.3c-0.5-0.2-0.9-0.5-1.3-0.8s-0.6-0.8-0.8-1.3c-0.2-0.5-0.3-1-0.3-1.6h2.3
+                            c0,0.2,0,0.5,0.1,0.7c0.1,0.2,0.2,0.4,0.4,0.6c0.2,0.2,0.3,0.3,0.6,0.4c0.2,0.1,0.4,0.1,0.7,0.1s0.5,0,0.7-0.1
+                            c0.2-0.1,0.4-0.2,0.6-0.4c0.2-0.2,0.3-0.3,0.4-0.6s0.1-0.4,0.1-0.7V7.5h2.3v9.1c0,0.6-0.1,1.1-0.3,1.6c-0.2,0.5-0.5,0.9-0.8,1.3
+                            s-0.8,0.6-1.3,0.8C38.1,20.5,37.6,20.6,37,20.6z"/>
+                            <path class="st0" d="M56.5,13.9c0,0.9-0.2,1.8-0.5,2.7s-0.8,1.5-1.5,2.1c-0.6,0.6-1.4,1.1-2.2,1.4c-0.8,0.3-1.7,0.5-2.7,0.5
+                            c-1,0-1.9-0.2-2.7-0.5c-0.8-0.3-1.6-0.8-2.2-1.4c-0.6-0.6-1.1-1.3-1.5-2.1s-0.5-1.7-0.5-2.7s0.2-1.8,0.5-2.7
+                            c0.4-0.8,0.8-1.5,1.5-2.1c0.6-0.6,1.3-1.1,2.2-1.4c0.8-0.3,1.7-0.5,2.7-0.5c0.9,0,1.8,0.2,2.7,0.5c0.8,0.3,1.6,0.8,2.2,1.4
+                            c0.6,0.6,1.1,1.3,1.5,2.1C56.3,12.1,56.5,13,56.5,13.9z M54,13.9c0-0.6-0.1-1.2-0.3-1.7c-0.2-0.5-0.5-1-0.9-1.4
+                            c-0.4-0.4-0.9-0.7-1.4-0.9c-0.5-0.2-1.1-0.3-1.7-0.3s-1.2,0.1-1.7,0.3c-0.5,0.2-1,0.5-1.4,0.9s-0.7,0.9-0.9,1.4
+                            c-0.2,0.5-0.3,1.1-0.3,1.7c0,0.6,0.1,1.2,0.3,1.7s0.5,1,0.9,1.4s0.9,0.7,1.4,0.9c0.5,0.2,1.1,0.3,1.7,0.3s1.2-0.1,1.7-0.3
+                            c0.5-0.2,1-0.5,1.4-0.9c0.4-0.4,0.7-0.9,0.9-1.4S54,14.5,54,13.9z"/>
+                            <path class="st0" d="M62.4,9.8v10.5h-2.4V9.8h-3.8V7.5h10v2.3H62.4z"/>
+                            <path class="st0" d="M76.9,20.3h-2.6L73.5,18h-4.8l-0.8,2.3h-2.6l4.5-12.8h2.6L76.9,20.3z M72.7,15.7l-1.5-5.2H71l-1.5,5.2H72.7z"
+                            />
+                            <path class="st0" d="M88.3,11.9c0,0.6-0.1,1.2-0.4,1.7c-0.2,0.5-0.6,1-1,1.4c-0.4,0.4-0.9,0.7-1.4,0.9c-0.5,0.2-1.1,0.3-1.8,0.3
+                            h-3.2v4.1h-2.4V7.5h5.6c0.6,0,1.2,0.1,1.8,0.3c0.5,0.2,1,0.5,1.4,0.9c0.4,0.4,0.7,0.8,1,1.4C88.2,10.7,88.3,11.2,88.3,11.9z
+                            M85.9,11.9c0-0.3-0.1-0.6-0.2-0.8c-0.1-0.2-0.3-0.5-0.4-0.6c-0.2-0.2-0.4-0.3-0.7-0.4c-0.3-0.1-0.5-0.2-0.8-0.2h-3.2v4.1h3.2
+                            c0.3,0,0.5-0.1,0.8-0.2s0.5-0.3,0.7-0.4c0.2-0.2,0.3-0.4,0.4-0.6S85.9,12.2,85.9,11.9z"/>
+                            <path class="st0" d="M99.4,18v2.3H90V7.5h9.4v2.3h-7v2.9h5.8v2.3h-5.8V18H99.4z"/>
+                        </g>
+                    </g>
+                    <g id="XMLID_44_">
+                        <path id="XMLID_45_" class="st0" d="M18.1,1.1v5.3h0l0,10v0c0,2.3-1.1,4.4-2.8,5.7c-0.6,0.4-1.2,0.8-1.9,1V20
+                        c1.2-0.8,1.9-2.1,1.9-3.6h0l0-5.7c0,0,0,0,0,0l0-4.3V0.6c-0.4,0-0.8,0-1.1,0c-7.7,0-14,6.3-14,14c0,6.3,4.2,11.7,10,13.4v-5.5h0
+                        l0-9.8v0c0-2.3,1.1-4.4,2.8-5.7c0.6-0.4,1.2-0.8,1.9-1v3.2C13.7,9.7,13,11,13,12.5h0l0,5.7c0,0,0,0,0,0l0,4.3v5.9
+                        c0.4,0,0.8,0,1.1,0c7.7,0,14-6.3,14-14C28.1,8.2,23.9,2.8,18.1,1.1z M9.8,23.5c-3.3-0.5-5.9-3.4-6.1-6.8C3.6,13.1,6.2,10,9.6,9.4
+                        C9.2,10.2,9,11.2,9,12.2v0l0,0.3c-1.5,0.7-2.5,2.2-2.5,3.9c0,2,1.4,3.7,3.2,4.2V23.5z M18.6,19.6c0.4-0.9,0.6-1.8,0.6-2.8v0l0-0.3
+                        c1.5-0.7,2.5-2.2,2.5-3.9c0-2-1.4-3.7-3.2-4.2V5.4c3.3,0.5,5.9,3.4,6.1,6.8C24.6,15.9,22,19,18.6,19.6z"/>
+                    </g>
+                </svg>
+                </a>
+
             </div>
         </div>
     </div>
+</div>
+</div>
 
 </footer>
 
